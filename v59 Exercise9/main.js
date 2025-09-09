ba8fca2b91e58ca0c0c53d42d7b4239cbf968efc
Vinayak Chinchakhandi <vinayakchinchakhandi165@gmail.com>
2025-09-09 22:15:51 +0530

    v59

---
+++ v59 Exercise9/main.js
@@ -0,0 +1,52 @@
+console.log("Faulty calculator");
+let random = Math.random();
+
+function faultycalculator() {
+    const r = require("readline");
+
+    const rl = r.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question("Enter first operand: ", (a) => {
+        rl.question("Enter second operand: ", (b) => {
+            rl.question("Enter operator (+, -, *, /): ", (c) => {
+                a = Number(a);
+                b = Number(b);
+
+                if (random < 0.1) {
+                    // faulty logic
+                    if (c === "+") {
+                        console.log(a - b);
+                    } else if (c === "-") {
+                        console.log(a + b);
+                    } else if (c === "*") {
+                        console.log(a / b);
+                    } else if (c === "/") {
+                        console.log(a ** b);
+                    } else {
+                        console.log("Enter valid operator.");
+                    }
+                } else {
+                    // normal logic
+                    if (c === "+") {
+                        console.log(a + b);
+                    } else if (c === "-") {
+                        console.log(a - b);
+                    } else if (c === "*") {
+                        console.log(a * b);
+                    } else if (c === "/") {
+                        console.log(a / b);
+                    } else {
+                        console.log("Enter valid operator.");
+                    }
+                }
+
+                rl.close();
+            });
+        });
+    });
+}
+
+faultycalculator();
