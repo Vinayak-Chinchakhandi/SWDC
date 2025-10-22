e03f1410a7d0023fc9bcae47bb7c23426902b122
Vinayak Chinchakhandi <vinayakchinchakhandi165@gmail.com>
2025-10-22 13:34:00 +0530

    v82

--- v82 Advanced JavaScript/script.js
+++ v82 Advanced JavaScript/script.js
@@ -43,6 +43,7 @@ async function sleep(){
 })()
 
 //===================================Spread Syntax============================================
+
 function sum(a,b,c){
     return a+b+c
 }
@@ -54,3 +55,23 @@ console.log(obj)     //{0: 1, 1: 4, 2: 6}
 
 console.log(arr[0]+arr[1]+arr[2])   //general method            =>11
 console.log(sum(...arr))    //...arr means spread array arr     =>11
+
+//========================================Hoisting===========================================
+
+console.log(a1)
+var a1=6   //undefined because only var a1 is sent above not a1=6
+//if we use let then will get error but we can declare a1 as var but it will be undefined 
+//this can be possible only in the specific scope
+
+//suppose var a1 is declared in one function 
+//console.log is written (accessed) outside the function then we will get error
+
+hi() //this can be done as function is moved above when called before defining
+function hi(){
+    console.log("Hi")
+}
+
+console.log(h1)   //here we cannot access h1 as it is declared as const
+const h1= hello(()=>{
+console.log("Hello")
+})
